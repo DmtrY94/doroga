cd918e7a1d5fe6eff88fc59deee94f9c2c510d0b
DmtrY94 <dmitriyyolov94@gmail.com>
2021-04-01 20:07:27 +0300

    initial commit

---
+++ components/Pagination.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="pagination">
+    <a v-if="hasPreviousPage" @click="paginate(-1)" class="prev"
+      >← Previous Page</a
+    >
+    <a v-if="hasNextPage" @click="paginate(+1)" class="next">Next Page →</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  data() {
+    return {
+      firstStartCursor: this.posts.pageInfo.startCursor,
+      hasNextPage: this.posts.pageInfo.hasNextPage,
+      hasPreviousPage: false,
+      first: null,
+      last: null,
+      startCursor: null,
+      endCursor: null,
+    };
+  },
+  props: {
+    posts: {
+      type: Object,
+    },
+    postsQuery: {
+      type: Object,
+    },
+    limit: {
+      type: Number,
+    },
+    where: {
+      type: Object,
+    },
+  },
+  methods: {
+    async paginate(i) {
+      if (i < 0) {
+        this.first = null;
+        this.last = this.limit;
+        this.startCursor = this.posts.data.posts.pageInfo.startCursor;
+        this.endCursor = null;
+      } else if (i > 0) {
+        this.first = this.limit;
+        this.last = null;
+        this.startCursor = null;
+        this.endCursor = this.posts.data.posts.pageInfo.endCursor;
+      }
+      await this.$apolloProvider.defaultClient
+        .query({
+          query: this.postsQuery,
+          variables: {
+            first: this.first,
+            last: this.last,
+            endCursor: this.endCursor,
+            startCursor: this.startCursor,
+            where: this.where,
+          },
+        })
+        .then(posts => {
+          this.$emit('updatePosts', posts);
+          if (i < 0) {
+            this.hasNextPage = true;
+          } else {
+            this.hasPreviousPage = true;
+            this.hasNextPage = posts.data.posts.pageInfo.hasNextPage;
+          }
+        });
+    },
+  },
+};
+</script>
